b498ef2ee3b903924f370b128dc4732ae99b6569
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2023-12-04 09:41:31 +0100

    Day 4: Cleanup

--- src/day04-scratchcards.py
+++ src/day04-scratchcards.py
@@ -4,51 +4,37 @@ from collections import defaultdict
 today = day(2023, 4)
 
 
-def find_matches(winning, have):
-  m = 0
-  for n in have:
-    if n in winning:
-      m += 1
-  return m
-
-
-def scratchcard_points(inp):
-  s = 0
-  lookup = dict()
-  for idx, winning, have in inp:
-    matches = find_matches(winning, have)
-    lookup[idx] = matches
-    if matches > 0:
-      v = 1
-      for _ in range(matches - 1):
-        v *= 2
-      s += v
-  return s, lookup
-
-
 def scratchcards_won(lookup):
   copies = defaultdict(int)
   for idx, matches in lookup.items():
     copies[idx] += 1
-    n = copies[idx]
     for i in range(idx+1, idx+matches+1):
-      copies[i] += n
+      copies[i] += copies[idx]
   return sum(copies.values())
 
 
+def score(match_count):
+  v = 1 if match_count else 0
+  for _ in range(match_count - 1):
+    v *= 2
+  return v
+
+
+def scratchcard_matches(cards):
+  return {idx: len(have & winning) for idx, winning, have in cards}
+
+
 def parse(line):
-  idx, rest = line.split(': ')
+  idx, rest = line.split(':')
   idx = int(idx[4:].strip())
-  winning, have = rest.split(' | ')
-  winning = list(map(int, winning.strip().replace('  ', ' ').split(' ')))
-  have = list(map(int, have.strip().replace('  ', ' ').split(' ')))
-  return (idx, winning, have)
+  winning, have = rest.split('|')
+  return idx, set(winning.split()), set(have.split())
 
 
 def main():
-  inp = list(get_data(today, [('func', parse)]))
-  star1, lookup = scratchcard_points(inp)
-  print(f'{today} star 1 = {star1}')
+  scratchcard_list = get_data(today, [('func', parse)])
+  lookup = scratchcard_matches(scratchcard_list)
+  print(f'{today} star 1 = {sum(map(score, lookup.values()))}')
   print(f'{today} star 2 = {scratchcards_won(lookup)}')
 
 
