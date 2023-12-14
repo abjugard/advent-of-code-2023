752d47e02d050c70a261e72e2dd771f4a506f67d
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2023-12-14 10:14:14 +0100

    Day 14: Use points

--- src/day14-parabolic_reflector_dish.py
+++ src/day14-parabolic_reflector_dish.py
@@ -5,27 +5,26 @@ from santas_little_utils import build_dict_map
 today = day(2023, 14)
 
 
-def tilt(rocks, the_map, direction='N'):
+def tilt(rocks, the_map, direction):
   rocks = set(rocks)
   cant_move = set()
   last_moved = True
 
   def locked(rock):
-    nr = Point(*rock).next(direction).t
+    nr = rock.next(direction)
     is_locked = nr not in the_map or the_map[nr] == '#' or nr in cant_move
     if is_locked:
       cant_move.add(rock)
     return is_locked
 
   def can_move(rock):
-    nr = Point(*rock).next(direction).t
+    nr = rock.next(direction)
     return not locked(rock) and not (nr in rocks or nr in n_rocks)
 
   def move(rock):
-    rock = Point(*rock)
-    while can_move(rock.t):
+    while can_move(rock):
       rock = rock.next(direction)
-    return rock.t
+    return rock
 
   while last_moved:
     last_moved = False
@@ -42,7 +41,6 @@ def tilt(rocks, the_map, direction='N'):
 def spin_cycle(the_map, h, rocks):
   c = 0
   seen = dict()
-
   cycle_found = False
   while c < 1e9:
     for d in 'NWSE':
@@ -54,7 +52,7 @@ def spin_cycle(the_map, h, rocks):
         c += skip * ((1e9-c) // skip)
         cycle_found = True
       seen[rocks] = c
-  return sum(h-y for _, y in rocks)
+  return sum(h-r.y for r in rocks)
 
 
 def init_rocks(the_map):
@@ -67,8 +65,9 @@ def init_rocks(the_map):
 
 def main():
   the_map, (_, h) = build_dict_map(get_data(today))
+  the_map = {Point(*p): c for p, c in the_map.items()}
   rocks = init_rocks(the_map)
-  print(f'{today} star 1 = {sum(h-y for _, y in tilt(rocks, the_map))}')
+  print(f'{today} star 1 = {sum(h-r.y for r in tilt(rocks, the_map, "N"))}')
   print(f'{today} star 2 = {spin_cycle(the_map, h, rocks)}')
 
 
