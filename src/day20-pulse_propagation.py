aeabe2592e8fa75f15acb39af106957c7750465b
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2023-12-20 20:03:17 +0100

    Day 20: Improved

--- src/day20-pulse_propagation.py
+++ src/day20-pulse_propagation.py
@@ -5,71 +5,67 @@ from math import lcm
 
 today = day(2023, 20)
 
-presses = 0
 rx_invoker_period = dict()
 pulse_counter = defaultdict(int)
 
 
-def run_program(state, program, rx_invoker):
+def run_program(state, program, rx_invoker, presses):
   pulses = deque([('button', 'broadcaster', False)])
   while pulses:
-    invoker, name, pulse_kind = pulses.popleft()
+    invoker, target, pulse_kind = pulses.popleft()
     pulse_counter[pulse_kind] += 1
-    if name not in program:
+    if target not in program:
       continue
-    kind, targets = program[name]
+    kind, targets = program[target]
     match kind:
       case 'broadcaster':
         for t in targets:
-          pulses.append((name, t, pulse_kind))
+          pulses.append((target, t, pulse_kind))
       case '%':
         if pulse_kind is False:
-          curr = state[kind][name] = not state[kind][name]
+          curr = state[kind][target] = not state[kind][target]
           for t in targets:
-            pulses.append((name, t, curr))
+            pulses.append((target, t, curr))
       case '&':
-        if name == rx_invoker and invoker not in rx_invoker_period and pulse_kind:
+        if target == rx_invoker and invoker not in rx_invoker_period and pulse_kind:
           rx_invoker_period[invoker] = presses
-        state[kind][name][invoker] = pulse_kind
-        next_pulse_kind = not all(s for s in state[kind][name].values())
+        state[kind][target][invoker] = pulse_kind
+        next_pulse_kind = not all(state[kind][target].values())
         for t in targets:
-          pulses.append((name, t, next_pulse_kind))
+          pulses.append((target, t, next_pulse_kind))
 
 
 def setup_state(program):
   state = {'%': defaultdict(bool), '&': defaultdict(dict)}
   for invoker, (_, targets) in program.items():
-    for t in targets:
-      if not t in program:
+    for target in targets:
+      if not target in program:
         rx_invoker = invoker
         continue
-      kind, _ = program[t]
-      if kind == '&':
-        state['&'][t][invoker] = False
-  return state, rx_invoker
+      if '&' in program[target]:
+        state['&'][target][invoker] = False
+  return state, rx_invoker, len(state['&'][rx_invoker])
 
 
 def star_gen(program):
-  global presses
-  state, rx_invoker = setup_state(program)
+  state, rx_invoker, rx_invoker_count = setup_state(program)
+  presses = 0
 
-  while len(rx_invoker_period) < 4:
+  while True:
     presses += 1
-    run_program(state, program, rx_invoker)
+    run_program(state, program, rx_invoker, presses)
     if presses == 1000:
       yield mul(pulse_counter.values())
-
-  yield lcm(*rx_invoker_period.values())
+    if len(rx_invoker_period) == rx_invoker_count:
+      yield lcm(*rx_invoker_period.values())
 
 
 def parse(line):
   symbol = line[0]
   name, targets = line[1:].split(' -> ')
   if name == 'roadcaster':
-    name = 'broadcaster'
-    symbol = 'broadcaster'
-  targets = targets.split(', ')
-  return symbol, name, targets
+    name = symbol = 'broadcaster'
+  return symbol, name, targets.split(', ')
 
 
 def main():
